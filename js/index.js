cf443367d0a662f0e56bb83a534dad28abc68ae5
barbielee <barbielee@staff.ipevo.com>
2015-12-16 21:22:47 +0800

    [121615] 前四個單元切版、程式

--- js/index.js
+++ js/index.js
@@ -1,51 +1,85 @@
+var now,pre,scrolling = false;
+var section =  $('section');
 var initial = {
 	initfun: function(){
 		navfun.navclick();
+		for(var i = 0; i < section.length+1; i++){
+			if($('body').scrollTop() < section.eq(i).offset().top+section.height()){
+				now = i;
+				break;
+			} 
+		}
 		scrollfun.navchange();
 	}
 }
-var now = 0,pre = 0,scrolling = false;
-var section =  $('section');
 var navfun = {
 	navall: $('nav'),
 	navclick: function(){
 		navfun.navall.find('ul li').on('click',function(){
 			var index = $(this).index();
 			scrolling = true;
-			console.log(scrolling,index);
-			$('body').animate({scrollTop: section.eq(index).offset().top},800,'easeInOutCubic',function(){
+				console.log(index+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+			$('body').animate({scrollTop: section.eq(index).offset().top-200},800,'easeInOutCubic',function(){
 				now = index;
-				console.log(scrolling,now);
-			});
 				scrolling = false;
+			});
 		});
 	}
 }
 
 
 var scrollfun = {
-	scrollpre: 0,
+	sec3delta: 0,
 	navchange: function(){
-		if(!$('body').is(':animated') && !scrolling){
-			$(window).scroll(function(){
-			// console.log($(this).scrollTop());
-				if($(this).scrollTop() > scrollfun.scrollpre){ //scroll down
+		$('.wrapper').mousewheel(function(event, delta){
+			 //console.log(now,$('body').scrollTop(),section.eq(now+1).offset().top, scrollfun.sec3delta);
+			//if (now!=3) {
+				event.preventDefault();				
+			//};
+			if(!$('body').is(':animated') && !scrolling){
+				if( delta < 0){ //scroll down
 					if( now < section.length-1){
-					now ++;
-					console.log(now);
-					$('nav ul li').eq(now).trigger('click');
-					console.log(pre,now);				
+					if(now ==3){
+						scrollfun.sec3delta += delta*40;
+						$('.content4_1').css({'top':scrollfun.sec3delta});
+						if(scrollfun.sec3delta < -2350){
+							now ++;
+						}
+							// console.log()
+						
+						//$(this).unbind('mousewheel');
+						//scrollfun.navchange();
+					}else{
+						if( now < 4 || scrollfun.sec3delta ==0){
+							now ++;
+						}
+						scrollfun.sec3delta = 0;
+						$('.content4_1').css({'top':scrollfun.sec3delta});
+						$('nav ul li').eq(now).trigger('click');			
+					}
 					pre = now;
 					}
-					scrollfun.scrollpre = $(this).scrollTop();
-				}else if($(this).scrollTop()<scrollfun.scrollpre){ //scroll up
-					//console.log('-');
-					scrollfun.scrollpre = $(this).scrollTop();
-				}			
-			});
-		}
+				} else if ( delta > 0 ){ //scroll up
+					if( now > 0) {
+                        pre = now;
+                        now --;
+                        $('nav ul li').eq(now).trigger('click');
+                    }
+				}
+			}
+			
+		});
 	}
 }
 $(window).load(function(){
+	// $images.imagesLoaded().progress(function(instance, image){
+ //        var result = image.isLoaded ? 'loaded' : 'broken';
+ //        count++;
+ //        if(count == $images.length){
+ //            // $.Loading.fadeOut(300);
+ //            // $.Body.MainDataInIt();
+ //        }
+ //    });
+
 	initial.initfun();
 });
