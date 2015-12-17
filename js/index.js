f6ca74a3be0971871ab58dabc0967314759f0042
barbielee <barbielee@staff.ipevo.com>
2015-12-17 17:17:06 +0800

    [121715] 前四個單元大致完成

--- js/index.js
+++ js/index.js
@@ -1,61 +1,96 @@
 var now,pre,scrolling = false;
+var ww = $(window).width(), wh = $(window).height();
 var section =  $('section');
 var initial = {
 	initfun: function(){
-		navfun.navclick();
 		for(var i = 0; i < section.length+1; i++){
 			if($('body').scrollTop() < section.eq(i).offset().top+section.height()){
 				now = i;
+				$('nav li').eq(now).find('span').css({'display':'block'})
 				break;
 			} 
 		}
+		clickfun.navfun();
+		clickfun.bar3fun();
 		scrollfun.navchange();
 	}
 }
-var navfun = {
+var clickfun = {
 	navall: $('nav'),
-	navclick: function(){
-		navfun.navall.find('ul li').on('click',function(){
+	navfun: function(){
+		clickfun.navall.find('ul li').on('click',function(){
 			var index = $(this).index();
 			scrolling = true;
-				console.log(index+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-			$('body').animate({scrollTop: section.eq(index).offset().top-200},800,'easeInOutCubic',function(){
+			$(this).find('span').css({'display':'block'}).parent().siblings().find('span').removeAttr('style');
+			$('body').stop().animate({scrollTop: section.eq(index).offset().top-(wh-section.height())/2},800,'easeInOutCubic',function(){
 				now = index;
 				scrolling = false;
+				$('.bg4_1').fadeIn(1000);
+				$('.sliders').removeAttr("style");
+				$('.content4_1').css({'top':scrollfun.sec3delta});
 			});
 		});
+	},
+	bar3fun: function(){
+		$('.bar3btn').on('click',function(){
+			 $('.car img').attr('src','img/'+$(this).attr('car')+'.png');
+			 $(this).find('div').css({'display':'block'}).parent().siblings().find('div').removeAttr('style');
+		})
 	}
 }
 
-
 var scrollfun = {
 	sec3delta: 0,
+	slider: $('.slider').width()/-2,
 	navchange: function(){
+		var temp = 0;
 		$('.wrapper').mousewheel(function(event, delta){
-			 //console.log(now,$('body').scrollTop(),section.eq(now+1).offset().top, scrollfun.sec3delta);
-			//if (now!=3) {
 				event.preventDefault();				
-			//};
 			if(!$('body').is(':animated') && !scrolling){
 				if( delta < 0){ //scroll down
+					console.log(temp,now);
 					if( now < section.length-1){
-					if(now ==3){
+					if(now == 1){
+						scrollfun.sec3delta += delta*10;
+						if(scrollfun.sec3delta < -200){
+							now ++;
+							temp = 3;
+						}else if(scrollfun.sec3delta < -120){
+							$('.sliders').stop();
+							$('.sliders').animate({'margin-left': scrollfun.slider*5},400);
+						}else if(scrollfun.sec3delta < -30){
+							$('.sliders').stop();
+							$('.sliders').animate({'margin-left': scrollfun.slider*3},400);
+					
+						}
+					}else if(now ==3){
 						scrollfun.sec3delta += delta*40;
 						$('.content4_1').css({'top':scrollfun.sec3delta});
+						if(scrollfun.sec3delta < -1120 && $('.bg4_1').width()){
+							$('.bg4_1').fadeOut(1000);
+							
+						}
 						if(scrollfun.sec3delta < -2350){
 							now ++;
+							temp = 3;
 						}
-							// console.log()
-						
+						// console.log()
 						//$(this).unbind('mousewheel');
 						//scrollfun.navchange();
 					}else{
-						if( now < 4 || scrollfun.sec3delta ==0){
-							now ++;
+						if( now != 4 && now != 2 || scrollfun.sec3delta == 0){
+							temp ++;
+							if( temp == 3 ){
+								now++;
+								$('nav ul li').eq(now).trigger('click');
+								temp = 0;		
+							}	
+						}else{
+							$('nav ul li').eq(now).trigger('click');
+							temp = 0;	
 						}
 						scrollfun.sec3delta = 0;
-						$('.content4_1').css({'top':scrollfun.sec3delta});
-						$('nav ul li').eq(now).trigger('click');			
+						
 					}
 					pre = now;
 					}
